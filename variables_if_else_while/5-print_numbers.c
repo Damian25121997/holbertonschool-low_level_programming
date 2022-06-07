512a4dc8a4014213efabf4792a9347db2ebcb494
Damian Caetano <5100@holbertonschool.com>
2022-06-06 18:32:46 -0700

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print the numbers since 0 to 9
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
