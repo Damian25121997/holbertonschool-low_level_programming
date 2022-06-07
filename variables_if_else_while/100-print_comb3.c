26f6a8e4693a2a3933d4ddc723696d305dd9b4e1
Damian Caetano <5100@holbertonschool.com>
2022-06-07 06:16:16 -0700

    Task 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints a serie of numbers with commas
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+int i, x;
+for (i = '0'; i <= '9'; i++)
+{
+for (x = i ; x <= '9'; x++)
+if (i != j)
+putchar(i, j)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
