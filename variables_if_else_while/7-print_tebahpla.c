0432881dc5b5cc8225f8d2bacb351e1f94b855af
Damian Caetano <5100@holbertonschool.com>
2022-06-06 18:44:51 -0700

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabetic reverse
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+char i;
+for (i = 'z'; i <= 'a'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
