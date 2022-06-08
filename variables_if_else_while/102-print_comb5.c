978576528d91e6b11f0734d7af6c86ce0687529d
Damian Caetano <5100@holbertonschool.com>
2022-06-07 17:58:33 -0700

    Task 12

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -12,23 +12,23 @@ for (i = '0'; i <= '9'; i++)
 {
 for (x = '0' ; x <= '9'; x++)
 {
-for (z = '0' ; z <= '9'; z++)
+for (z = i ; z <= '9'; z++)
 {
-for (y = (z + 1) ; y <= '9'; y++)
+for (y = x ; y <= '9'; y++)
 {
-if (i == x && x != z && i != z)
+if (x != y)
 {
 putchar(i);
 putchar(x);
 putchar(' ');
 putchar(z);
 putchar(y);
-if
+/*if
 (i != '7' || x != '8' || z != '9')
 {
 putchar(',');
 putchar(' ');
-}
+}*/
 }
 }
 }
