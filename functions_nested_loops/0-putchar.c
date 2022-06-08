13488e138704563cec52ece9d908065089cc9287
Damian Caetano <5100@holbertonschool.com>
2022-06-08 08:51:24 -0700

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints a serie of numbers with commas
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+putchar('_')
+putchar('p')
+putchar('u')
+putchar('t')
+putchar('c')
+putchar('h')
+putchar('a')
+putchar('r')
+putchar('\n')
+return(0)
+}
