22a76ed3721b1f0da858ce54aaa3ab17d82f07be
Damian Caetano <5100@holbertonschool.com>
2022-06-09 11:35:56 -0700

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - add two integres and returns the result
+ *
+ * @a: integrer
+ *
+ * @b: integrer
+ *
+ * Return: Always
+ */
+
+int add (int a, int b)
+{
+	return (a + b);
+}
