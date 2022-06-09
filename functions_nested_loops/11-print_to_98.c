cd72859cb7514ba0091710df4b59dbd5596f3bee
Damian Caetano <5100@holbertonschool.com>
2022-06-09 15:30:17 -0700

    Task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all natural numbers
+ * followed by a new line
+ * @n: print from this number
+ */
+
+void print_to_98(int n)
+
+{
+	if (n < 98)
+	{
+		for (n < 98 ; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (n > 98 ; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+}
