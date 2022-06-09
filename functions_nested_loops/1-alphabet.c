185f58f24a8aa1e7e3a3bdb4755c54938b7ac334
Damian Caetano <5100@holbertonschool.com>
2022-06-08 18:35:05 -0700

    Task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always
+ */
+
+void print_alphabet(void)
+{
+	char a = 97;
+
+	while
+	(a <= 122);
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar(10);
+}
