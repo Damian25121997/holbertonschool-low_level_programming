f5dfbe03024774bce28f683b1565be51e0ba29cb
Damian Caetano <5100@holbertonschool.com>
2022-06-09 11:25:38 -0700

    Task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times tables
+ */
+
+void times_table(void)
+{
+	int n, i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1 ; j <= 9 ; j++)
+		{
+			n = i * j;
+			if ((n / 10) != 0)
+			{
+				_putchar((n / 10) + '0');
+			}
+			else
+			{
+				_putchar((' ');
+			}
+			_putchar((n % 10) + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}	
+
+
+
