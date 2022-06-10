ca80c205106d61c1608c50900560f011c7d7fd4d
Damian Caetano <5100@holbertonschool.com>
2022-06-10 05:24:08 -0700

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers, from 0 to 9, followed by new line
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 9)
+		{
+		_putchar(n);
+		}
+	n++;
+}
