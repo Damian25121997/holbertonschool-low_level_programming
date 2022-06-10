357cb0d5f4a7fcddebbfed30227b8a376a9c2869
Damian Caetano <5100@holbertonschool.com>
2022-06-10 06:53:20 -0700

    Task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: print 
+ */
+void print_line(int n)
+{
+	int m
+	if (n > 0)
+	{
+		for (m = 0; m < n; m++)
+			_putchar('_');
+	}	
+	_putchar('\n');
+}
