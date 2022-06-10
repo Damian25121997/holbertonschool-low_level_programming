713885d40d6b91530d0a64a504eec5db304a11d4
Damian Caetano <5100@holbertonschool.com>
2022-06-10 10:25:38 -0700

    Task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle, followed by a new line
+ * @size: character print
+ */
+
+void print_triangle(int size)
+{
+	int m, b;
+
+	if (size > 0)
+	{
+		for (m = 0; m < size; m--)
+		{
+		for (b = 0; b < size; b--)
+			_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
