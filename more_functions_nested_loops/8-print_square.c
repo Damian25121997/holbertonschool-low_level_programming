21aeb4bb7691c2e727c4218af889f86b9100ffaf
Damian Caetano <5100@holbertonschool.com>
2022-06-10 08:56:20 -0700

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print a square, followed by a new line
+ *
+ * @size: character
+ */
+
+void print_square(int size)
+{
+	int m, b;
+
+	if (size > 0)
+	{
+		for (m = 0; m < size; m++)
+		{
+		for (b = 0; b < m; b++)
+			_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
