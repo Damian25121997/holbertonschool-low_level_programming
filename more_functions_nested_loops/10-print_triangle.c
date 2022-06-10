be9e44ddfc01e32541d262e899041b8ebaf37cf1
Damian Caetano <5100@holbertonschool.com>
2022-06-10 11:12:12 -0700

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 	{
 	for (m = 0; m < size; m++)
 	{
-	for (b = 0; b < size; b++)
+	for (b = 1; b < size; b++)
 	{
 	if ( m < b)
 	_putchar(' ');
@@ -22,7 +22,6 @@ void print_triangle(int size)
 	}
 	_putchar('\n');
 	}
-	m--;
 	}
 	else
 	_putchar('\n');
