ad88c3a233f41b9c39f596977da29145f4944c7d
Damian Caetano <5100@holbertonschool.com>
2022-06-10 06:59:47 -0700

    Task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,15 +2,18 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: print 
+ *
+ * @n: print the character
+ *
  */
 void print_line(int n)
 {
 	int m;
+
 	if (n > 0)
 	{
 		for (m = 0; m < n; m++)
 			_putchar('_');
-	}	
+	}
 	_putchar('\n');
 }
