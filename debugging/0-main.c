b193c9100b85e1738e67d515305d1edd6e6633c0
Damian Caetano <5100@holbertonschool.com>
2022-06-10 15:47:18 -0700

    Task 0

---
+++ debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Test if number is positive or negative
+ *
+ * Return: Always
+ */
+
+int main(void)
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
