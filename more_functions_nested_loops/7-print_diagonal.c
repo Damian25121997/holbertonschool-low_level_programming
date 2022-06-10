d6234ab1cadee4a0e54206ab7bf31a3b78f74c93
Damian Caetano <5100@holbertonschool.com>
2022-06-10 07:24:11 -0700

    Task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_diagonal - daws a diagonal line on the terminal.
+ * print_diagonal - daws a diagonal line on the terminal
  *
  * @n: character
  */
@@ -9,15 +9,17 @@
 void print_diagonal(int n)
 {
 	int m,  b;
+
 	if (n > 0)
 	{
-	for (m = 0; m < n; m++)
+		for (m = 0; m < n; m++)
 		{
 			for (b = 0; b < m; b++)
 				_putchar(' ');
 				_putchar(92);
 				_putchar('\n');
 		}
-	_putchar('\n');
 	}
+	else
+	_putchar('\n');
 }
