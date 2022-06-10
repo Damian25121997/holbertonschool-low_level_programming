556ddaeba499b611b0facbf5c758909d9309fb70
Damian Caetano <5100@holbertonschool.com>
2022-06-09 18:06:18 -0700

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integres.
+ * @a: first value
+ * @b: sec value
+ * Return: Always
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
