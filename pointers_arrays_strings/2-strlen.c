db7bc0c7f4bbec91f84b66d5875a3767538492bf
Damian Caetano <5100@holbertonschool.com>
2022-06-14 10:38:25 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the lenht of a string
+ *
+ * @s: is a value
+ *
+ */
+
+int _strlen(char *s)
+{
+int x = 0;
+while(*s != '\0')
+	{
+	x++;
+	s++;
+	}
+return x;
+}
