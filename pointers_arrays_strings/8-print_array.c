a421c9bc14df2667ac4e51df487fd0218768d935
Damian Caetano <5100@holbertonschool.com>
2022-06-15 10:03:30 -0700

    Task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Print n elements of an array of integres
+ *
+ * @a: int type array pointer
+ * 
+ * @n: int type integrer
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int z;
+	
+	z = 0;
+	for (n--; n >= 0; n--, z++ )
+	{
+		printf("%d", a[z]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
