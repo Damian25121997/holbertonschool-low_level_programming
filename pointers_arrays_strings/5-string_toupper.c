728ddb01f60e9f4c2e2f15f41196035f1b21a733
Damian Caetano <5100@holbertonschool.com>
2022-06-16 11:33:19 -0700

    Task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters of uppercase
+ * @p: string
+ * Return: p
+ */
+
+char *string_toupper(char *p)
+{
+	int x;
+
+	for (x = 0; p[x] != '\0'; x++)
+	{
+		if (p[x] > 'a' && p[x] < 'z')
+		{
+			p[x] -= ' ';
+		}
+	}
+	return (p);
+}
