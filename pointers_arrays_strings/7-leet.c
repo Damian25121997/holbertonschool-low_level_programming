0a939efad298118a0a2df655ed5cadbb19b9a8cc
Damian Caetano <5100@holbertonschool.com>
2022-06-16 12:33:22 -0700

    Task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,19 @@
 
 char *leet(char *p)
 {
-	int x;
+	int x, z;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
 	for (x = 0; p[x] != '\0'; x++)
 	{
-		while (p[x] == 'a' || p[x] == 'A')
-			p[x] = '4';
-		while (p[x] == 'e' || p[x] == 'E')
-			p[x] = '3';
-		while (p[x] == 'o' || p[x] == 'O')
-			p[x] = '0';
-		while (p[x] == 't' || p[x] == 'T')
-			p[x] = '7';
-		while (p[x] == 'l' || p[x] == 'L')
-			p[x] = '1';
+		for (z = 0; z < 10; z++)
+		{
+			if (p[x] == a[z])
+			{
+				p[x] = b[z];
+			}
+		}
 	}
 	return (p);
 }
