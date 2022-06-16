3e1849dfd70522aee74874c0915e846f5c1ab463
Damian Caetano <5100@holbertonschool.com>
2022-06-16 09:10:55 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string 
+ *
+ * @dest: is a string
+ * @n: number of elements to concatenate
+ * @src: is a string
+ *
+ * Return: dest
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+
+	for ( ; x < n; x++)
+		dest[x] = '\0';
+
+	return (dest);
+}
