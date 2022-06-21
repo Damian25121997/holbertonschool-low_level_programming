77014076c99709ab9f5c41185d9c4c6155e64367
Damian Caetano <5100@holbertonschool.com>
2022-06-21 10:32:08 -0700

    Task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sdlib.h>
+
+/**
+* main - prints its name
+* @argc: stands for argument count
+* @argv: stands for argument values
+* Return: 0
+*/
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return(0);
+}
