da948cceed80d7c1c69a4d3071581423d71e0936
Damian Caetano <5100@holbertonschool.com>
2022-06-21 10:45:05 -0700

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints its name
+* @argc: stands for argument count
+* @argv: stands for argument values
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	argc > 1;
+	printf("%s\n", argc);
+
+	return (0);
+
+}
