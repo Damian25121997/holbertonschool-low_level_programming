963df56a3bbcc45f43ae85a981aa901ebae3344e
Damian Caetano <5100@holbertonschool.com>
2022-06-23 16:12:46 -0700

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *pointer;
 	int f1, f2;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	f1 = strlen(s1);
 	f2 = strlen(s2);
