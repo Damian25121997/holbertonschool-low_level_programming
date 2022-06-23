a27c8a930d3f27bdba3f28d20473cdf35fb50674
Damian Caetano <5100@holbertonschool.com>
2022-06-23 11:33:53 -0700

    Task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* _strdup - return a pointer to a newly allocated space in memory.
+* @str: array to copy
+* Return: pointer
+*/
+
+char *_strdup(char *str)
+{
+	char *pointer;
+	int size, x;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = strlen(str);
+	pointer = (char *)malloc(size * sizeof(char));
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < size; x++)
+	{
+		pointer[x] = str[x];
+	}
+	return (pointer);
+}
