7e726d60f30a7825a486c8a5544ca0803178780f
Damian Caetano <5100@holbertonschool.com>
2022-06-23 07:38:40 -0700

    Task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int z;
+	char *pointer;
+
+	if (size == 0)
+		
+	return (NULL);
+
+	pointer = (char *)malloc(size * sizeof(char));
+
+	for (z = 0; z < size; z++)
+	{
+		pointer[z] = c;
+	}
+	return (pointer);
+
+	if (pointer == NULL)
+	return (NULL);
+}
