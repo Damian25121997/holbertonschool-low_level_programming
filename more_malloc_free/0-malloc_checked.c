c2e0523a817ffbce61a38ca4c921e7a7cb3f265f
Damian Caetano <5100@holbertonschool.com>
2022-06-24 06:07:13 -0700

    Task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+* malloc_checked - allocates memory using
+* @b: value of variable
+* Return: the new pointer
+*/
 void *malloc_checked(unsigned int b)
 {
 	char *pointer;
