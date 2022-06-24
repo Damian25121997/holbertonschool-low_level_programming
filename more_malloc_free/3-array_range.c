0856aa6d1f10b7dd7618f8f973754a9a05bfc1d2
Damian Caetano <5100@holbertonschool.com>
2022-06-24 10:15:51 -0700

    Task 3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	int a, x;
+	char *pointer;
+
+	if (min > max)
+		return (NULL);
+
+	a = max - min + 1;
+
+	pointer = malloc(sizeof(int) * a)
+
+	if (p == NULL)
+		return (NULL);
+
+	for (x = 0; x <= a; x++)
+		pointer[x] = min + x;
+	return (pointer);
+}
