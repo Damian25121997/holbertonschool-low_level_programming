c196279f34b44ba109b92f19ae55fbc0fd422b18
Damian Caetano <5100@holbertonschool.com>
2022-06-29 10:59:07 -0700

    Task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,15 +19,15 @@ int main(int argc, char *argv[])
 	two = atoi(argv[3]);
 	get_op = argv[2];
 
-	if (get_op_func(argv[2]) == NULL && argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	if (((*get_op == '/') || (*get_op == '%')) && (two == '0'))
 	{
 		printf("Error\n");
 		exit(100);
+	}
+	if (get_op_func(argv[2]) == NULL && argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
 	}
 	
 	res = get_op_func(get_op);
