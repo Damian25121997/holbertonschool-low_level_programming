efc95a40e12a3635c2164998efe3c16bca8e7788
Damian Caetano <5100@holbertonschool.com>
2022-06-29 10:15:53 -0700

    Task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <sdtio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	int one, two, eq;
+	int (*res)(int, int);
+	char *get_op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func argv[2] == NULL || argv[2] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((get_op argv[2] == '/' || get_op argv[2] == '%') && two == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	eq = (*(get_op_func(get_op)))(one, two)
+
+	printf("%d\n", eq);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+* op_add - function
+* @a: input
+* @b: input
+* Return: Always
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - function
+* @a: input
+* @b: input
+* Return: Always
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - function
+* @a: input
+* @b: input
+* Return: Always
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - function
+* @a: input
+* @b: input
+* Return: Always
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - function
+* @a: input
+* @b: input
+* Return: Always
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
