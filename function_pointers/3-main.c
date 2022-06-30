6d5f2e14a5e01225343fb06b5049ec6edb42dd6f
Damian Caetano <5100@holbertonschool.com>
2022-06-29 17:00:36 -0700

    Task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+* main - main function
+* @argc: argument count
+* @argv: argument list
+* Return: result of function
+*/
 int main(int argc, char *argv[])
 {
-	int one, two, eq;
-	int (*res)(int, int);
+	int one, eq;
 	char *get_op;
 
 	if (argc < 4)
@@ -16,23 +20,22 @@ int main(int argc, char *argv[])
 	}
 
 	one = atoi(argv[1]);
-	two = atoi(argv[3]);
 	get_op = argv[2];
 
-	if ((*get_op == '/' || *get_op == '%') && (two == '0'))
+	if ((*get_op == '/' || *get_op == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	else if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
-	res = get_op_func(get_op);
-	eq = res(one, two);
-
-	printf("%d\n", eq);
-	return (0);
+	else
+	{
+		eq = (*(get_op_func(get_op)))(one, (atoi(argv[3])));
+		printf("%d\n", eq);
+		return (0);
+	}
 }
