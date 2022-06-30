bb9c28ae0e03c202c3c539a8d091f2611f1467ec
Damian Caetano <5100@holbertonschool.com>
2022-06-29 17:08:24 -0700

    Task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,12 +18,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
-			{
-				return (ops[i].f);
-			}
+		if (*(ops[i].op) == s[0])
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
 	return (NULL);
