78c4bb6d2d47151db9dd11b5830ccc80af77f842
Damian Caetano <5100@holbertonschool.com>
2022-07-01 09:51:02 -0700

    Task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,11 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	unsigned int x, sum = 0;
 
-	va_start (valist, n);
+	va_start(valist, n);
 	for (x = 0; x < n; x++)
 	{
 		sum += va_arg(valist, int);
 	}
-	va_end (valist);
+	va_end(valist);
 	return (sum);
 }
