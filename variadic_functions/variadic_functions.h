b246a9381ceea407e9177fa351cc29df69d43bf4
Damian Caetano <5100@holbertonschool.com>
2022-07-02 09:50:31 -0700

    Variadic_functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,4 +8,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct dt
+{
+	char letter;
+	void (*f)(va_list);
+} datatype;
 #endif
