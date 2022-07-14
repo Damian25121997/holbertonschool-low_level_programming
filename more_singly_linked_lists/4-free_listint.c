36a16c1d94022034922d3bf42af9b7409541d3e3
Damian Caetano <5100@holbertonschool.com>
2022-07-14 06:35:47 -0700

    Task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_listint - free the memory
+* @head: pointer
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
