dab6ffb991206124a8c18fe3af40bb09e58c7246
Damian Caetano <5100@holbertonschool.com>
2022-07-14 07:01:23 -0700

    Task 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
-/** free_listint - free the memory
+/**
+* free_listint - free the memory
 * @head: pointer of a pointer
 */
 
@@ -8,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		(*head) = temp;
 	}
-	(*head) = NULL;
 }
-
