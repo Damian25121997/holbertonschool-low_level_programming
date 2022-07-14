77802452fa24fe7dd3336ee35f12f26a75f10311
Damian Caetano <5100@holbertonschool.com>
2022-07-14 07:28:26 -0700

    Task 6

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -8,13 +8,16 @@
 
 int pop_listint(listint_t **head)
 {
+	int n;
+
 	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
+	n = (*head)->n;
 	temp = *head;
 	*head = (*head)->next;
 	free(temp);
-	return (0);
+	return (n);
 }
