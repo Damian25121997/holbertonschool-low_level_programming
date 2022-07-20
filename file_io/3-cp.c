18d683f586a540008ac4b1a65868426559fa721a
Damian Caetano <5100@holbertonschool.com>
2022-07-20 16:37:20 -0700

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,17 +27,7 @@ int main(int argc, char *argv[])
 	while (r == 1024)
 	{
 		r = read(f1, buffer, 1024);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
 		w = write(f2, buffer, r);
-		if (w < r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
 	}
 	if (close(f1) == -1)
 	{
