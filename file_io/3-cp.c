c34d9b9b13d3a476e3e5ade5b9a178fe77b182da
Damian Caetano <5100@holbertonschool.com>
2022-07-20 16:22:15 -0700

    Task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+int main(int argc, char *argv[])
+{
+	char buffer[1024];
+	int f1, f2, r, w;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f1 = open(argv[1], O_RDONLY);
+	if (f1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (r == 1024)
+	{
+		r = read(f1, buffer, 1024);
+		w = write(f2, buffer, r);
+	}
+	if (close(f1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", r);
+		exit(100);
+	}
+	if (close(f2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", w);
+		exit(100);
+	}
+	return (0);
+}
