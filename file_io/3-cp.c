ff1c74b02aceaa4cd6d6eb32a6d31e9bea9505cc
Damian Caetano <5100@holbertonschool.com>
2022-07-20 16:45:54 -0700

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	f1 = open(argv[1], O_RDONLY);
 	if (f1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (f2 == -1)
 	{
@@ -21,7 +22,8 @@ int main(int argc, char *argv[])
 	{
 		r = read(f1, buffer, 1024);
 		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		w = write(f2, buffer, r);
 		if (w < r)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
