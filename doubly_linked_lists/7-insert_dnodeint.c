b294a54a55e27c0a6e2713f385ea5700a444502f
Damian Caetano <5100@holbertonschool.com>
2022-07-21 17:11:33 -0700

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new->next = temp->next;
 			new->prev = temp;
-			temp->next->prev = new
+			temp->next->prev = new;
 			temp->next = new;
 			return (new);
 		}
